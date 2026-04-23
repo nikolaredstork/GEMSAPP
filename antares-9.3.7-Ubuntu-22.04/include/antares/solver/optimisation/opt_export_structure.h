d5da2abec0cfc312e2096457a24bf52a2c8d1883
nikolaredstork <nikola.ilic@redstork-solutions.com>
2026-04-23 21:52:08 +0200

    exclude solver

--- antares-9.3.7-Ubuntu-22.04/include/antares/solver/optimisation/opt_export_structure.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2007-2026, RTE (https://www.rte-france.com)
-// SPDX-License-Identifier: MPL-2.0
-
-#pragma once
-
-#include "antares/writer/i_writer.h"
-struct PROBLEME_HEBDO;
-
-void OPT_ExportStructures(PROBLEME_HEBDO* problemeHebdo, Antares::Solver::IResultWriter& writer);
--- antares-9.3.7-Ubuntu-22.04/include/antares/study/constraint.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2007-2026, RTE (https://www.rte-france.com)
-// SPDX-License-Identifier: MPL-2.0
-
-#ifndef __ANTARES_LIBS_STUDY_CONSTRAINT_H__
-#define __ANTARES_LIBS_STUDY_CONSTRAINT_H__
-
-#include "binding_constraint/BindingConstraint.h"
-
-#endif // __ANTARES_LIBS_STUDY_CONSTRAINT_H__
